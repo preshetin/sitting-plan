1e021bf9196b2707850b8695f418f1fe1f610c90
Petr Reshetin <preshetin@gmail.com>
2023-01-16 17:02:13 +0500

    static site

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,12 +1,24 @@
-import { StackContext, Api } from "@serverless-stack/resources";
+import { StackContext, Api, StaticSite } from "@serverless-stack/resources";
 
 export function MyStack({ stack }: StackContext) {
   const api = new Api(stack, "api", {
     routes: {
       "GET /": "functions/lambda.handler",
+      "GET /translate": "functions/lambda.handler",
     },
   });
+
+  const site = new StaticSite(stack, "react", {
+    path: "frontend",
+    buildOutput: "dist",
+    buildCommand: "yarn build",
+    environment: {
+      VITE_API_URL: api.url,
+    },
+  });
+
   stack.addOutputs({
     ApiEndpoint: api.url,
+    WebSite: site.url
   });
 }
